3e54d3e0b905d23c0480999d6414c699ead26958
Meli093 <31870468+Meli093@users.noreply.github.com>
2019-03-07 23:57:20 -0600

    Add files via upload
    Este es el bueno

---
+++ Login/Repasndo-C-FORMS/Login/Login/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //vamoa a poner nuestro conteo.
+            int cont = 0;
+            string seleccion;
+
+            if (checkBox1.Checked == true)
+            {
+                cont ++;
+            }
+            if (checkBox2.Checked == true)
+            {
+                cont++;
+            }
+            if (radioButton1.Checked == true)
+            {
+                seleccion = "Tarjeta de credito";
+            }
+            else
+            {
+                seleccion = "paypal";
+            }
+            MessageBox.Show("has seleccionado" + cont + "el curso y el pago es:" + seleccion);
+        }
+        }
+    }
+}
---
+++ Login/Repasndo-C-FORMS/README.md
@@ -0,0 +1,1 @@
+# Repasndo-C-FORMS
